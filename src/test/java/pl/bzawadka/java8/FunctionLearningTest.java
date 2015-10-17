8fe497c610b2b99a77c722fb39df6b1d8198f802
Bartlomiej Zawadka <bartlomiej.zawadka@gmail.com>
2015-10-17 23:54:44 +0200

    Java8: learning BiFunction

--- src/test/java/pl/bzawadka/java8/FunctionLearningTest.java
+++ src/test/java/pl/bzawadka/java8/FunctionLearningTest.java
@@ -16,15 +16,28 @@ import static org.junit.Assert.assertThat;
 public class FunctionLearningTest {
 
     @Test
-    public void testFunctionInHashSetWorks() {
-        Function<Department, Set<Person>> defaultHrPersonsFunction = d -> new HashSet<>();
+    public void testFunctionInHashMapWorks() {
+        Function<Department, Set<Person>> noPersonsForDepartment = d -> new HashSet<>();
 
         Map<Department, Set<Person>> personsByDepartments = new HashMap<>();
         assertThat(personsByDepartments.size(), is(0));
 
-        personsByDepartments.computeIfAbsent(Department.HR, defaultHrPersonsFunction);
+        personsByDepartments.computeIfAbsent(Department.HR, noPersonsForDepartment);
         assertThat(personsByDepartments.size(), is(1));
         assertThat(personsByDepartments.get(Department.HR).isEmpty(), is(true));
+    }
+
+    @Test
+    public void testBiFunctionInHashMapWorks() {
+        Map<String, String> map = new HashMap<>();
+        map.put("foo", null);
+        map.put("bar", "BAR");
+
+        map.compute("foo", (k, v) -> v == null ? "FRESH" : v.concat(" STALE")); // BiFunction<String, String, String>
+        map.compute("bar", (k, v) -> v == null ? "FRESH" : v.concat(" STALE"));
+
+        assertThat(map.get("foo"), is("FRESH"));
+        assertThat(map.get("bar"), is("BAR STALE"));
     }
 
 }
